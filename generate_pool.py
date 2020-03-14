6e94637f86fbe60d80088934f422e74ed46e780a
Andrew Booker <improvizone@googlemail.com>
2020-03-14 17:42:34 +0000

    working out file index based on highest file number in directory

--- generate_pool.py
+++ generate_pool.py
@@ -4,6 +4,16 @@ import soundfile as sf
 import sys
 import os
 from math import floor
+import re
+
+def findStartIdx(inDir):
+	existingFiles = os.listdir(inDir)
+	if len(existingFiles) == 0:
+		return 0
+	highestFn = sorted(existingFiles)[-1]
+	regex = re.compile(r'\d+')
+	return 1 + int(regex.findall(highestFn)[0])
+	
 
 if len(sys.argv) < 4:
 	print("./generate_pool.py <dir> <source file> <tempo> <beats per sample>")
@@ -12,7 +22,7 @@ if len(sys.argv) < 4:
 
 subDir = "pool"
 workingDir = sys.argv[1]
-existingFileCount = len(os.listdir(os.path.join(workingDir, subDir)))
+startIdx = findStartIdx(os.path.join(workingDir, subDir))
 
 srcFn = os.path.join(workingDir, sys.argv[2])
 if not os.path.isfile(srcFn):
@@ -27,7 +37,7 @@ data, sampleRate = sf.read(srcFn)
 sampleLength = int(floor(sampleRate * (beats + beatsOverlap) * secsPerBeat))
 
 for i in range(1):
-	fnOut = os.path.join(workingDir, "pool\\%s_%.3d.wav" % (subDir, i + existingFileCount))
+	fnOut = os.path.join(workingDir, "pool\\%s_%.3d.wav" % (subDir, i + startIdx))
 	print("generating %s" % fnOut)
 	with sf.SoundFile(fnOut, mode="x", samplerate=sampleRate, channels=1, subtype="PCM_24") as outFile:
 		outFile.write(data[:sampleLength])
