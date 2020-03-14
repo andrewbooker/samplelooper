75d93f5d3b715ad71337fddf0f996b291c21e830
Andrew Booker <improvizone@googlemail.com>
2020-03-14 10:04:58 +0000

    sampling the first given number of beats of the input wav

--- generate_pool.py
+++ generate_pool.py
@@ -3,16 +3,31 @@
 import soundfile as sf
 import sys
 import os
+from math import floor
 
-if len(sys.argv) < 5:
-	print("./generate_pool.py <dir> <source file> <tempo> <beats per sample> <number of samples>")
-	print("eg ./generate_pool.py /samples input.wav 120 4 10")
+if len(sys.argv) < 4:
+	print("./generate_pool.py <dir> <source file> <tempo> <beats per sample>")
+	print("eg ./generate_pool.py /samples input.wav 120 4")
 	exit()
 
-srcFn = os.path.join(sys.argv[1], sys.argv[2])
+workingDir = sys.argv[1]
+srcFn = os.path.join(workingDir, sys.argv[2])
 if not os.path.isfile(srcFn):
 	print("source file '%s' not found" % srcFn)
 	exit()
+
+secsPerBeat = 60.0 / int(sys.argv[3])
+beats = int(sys.argv[4])
+
+data, sampleRate = sf.read(srcFn)
+sampleLength = int(floor(sampleRate * beats * secsPerBeat))
+
+for i in range(1):
+	fnOut = os.path.join(workingDir, "pool\\loop_%.3d.wav" % i)
+	print("generating %s" % fnOut)
+	with sf.SoundFile(fnOut, mode="x", samplerate=sampleRate, channels=1, subtype="PCM_24") as outFile:
+		outFile.write(data[:sampleLength])
+		
 
 print("done")
 
