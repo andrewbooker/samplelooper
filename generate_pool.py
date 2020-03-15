31b2957bc186df6100ee5e9efef98874e513612c
Andrew Booker <improvizone@googlemail.com>
2020-03-15 12:21:38 +0000

    switching to naming file according to source file and start position, binning existing file index lookup

--- generate_pool.py
+++ generate_pool.py
@@ -6,41 +6,38 @@ import os
 from math import floor
 import re
 
-def findStartIdx(inDir):
-	existingFiles = os.listdir(inDir)
-	if len(existingFiles) == 0:
-		return 0
-	highestFn = sorted(existingFiles)[-1]
-	regex = re.compile(r'\d+')
-	return 1 + int(regex.findall(highestFn)[0])
-	
+regex = re.compile(r'\d+')
 
-if len(sys.argv) < 5:
-	print("./generate_pool.py <dir> <source file> <tempo> <beats per sample>")
-	print("eg ./generate_pool.py /samples input.wav 120 4")
+
+if len(sys.argv) < 6:
+	print("./generate_pool.py <dir> <source file> <tempo> <beats per sample> <samples>")
+	print("eg ./generate_pool.py /samples input.wav 120 4 6")
 	exit()
 
 subDir = "pool"
 workingDir = sys.argv[1]
-startIdx = findStartIdx(os.path.join(workingDir, subDir))
 
-srcFn = os.path.join(workingDir, sys.argv[2])
+inFileName = sys.argv[2]
+srcFn = os.path.join(workingDir, inFileName)
 if not os.path.isfile(srcFn):
 	print("source file '%s' not found" % srcFn)
 	exit()
 
 secsPerBeat = 60.0 / int(sys.argv[3])
-beats = int(sys.argv[4])
+beatsPerBar = int(sys.argv[4])
 beatsOverlap = 0.05
+required = int(sys.argv[5])
 
 data, sampleRate = sf.read(srcFn)
-sampleLength = int(floor(sampleRate * (beats + beatsOverlap) * secsPerBeat))
+sampleLength = int(floor(sampleRate * (beatsPerBar + beatsOverlap) * secsPerBeat))
 
-for i in range(1):
-	fnOut = os.path.join(workingDir, "pool\\%s_%.3d.wav" % (subDir, i + startIdx))
+for i in range(required):
+	startTime = i * beatsPerBar * secsPerBeat
+	fnOut = os.path.join(workingDir, "%s\\%s_%08.4f.wav" % (subDir, inFileName.split(".")[0], startTime))
+	startSample = int(floor(sampleRate * startTime))
 	print("generating %s" % fnOut)
 	with sf.SoundFile(fnOut, mode="x", samplerate=sampleRate, channels=1, subtype="PCM_24") as outFile:
-		outFile.write(data[:sampleLength])
+		outFile.write(data[startSample:(sampleLength + startSample)])
 		
 
 print("done")
